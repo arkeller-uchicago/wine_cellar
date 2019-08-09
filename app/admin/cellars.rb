834a8d2bc73affddbb5c4c5354d74616582d74ec
Raghu Betina <raghu@firstdraft.com>
2019-08-09 16:55:17 +0000

    Whitelist columns in Cellar for ActiveAdmin

--- app/admin/cellars.rb
+++ app/admin/cellars.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Cellar do
+
+ permit_params :user_id, :wine_id, :transactions_id, :amount, :vitage_id, :remark, :size_id, :trans_date
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
